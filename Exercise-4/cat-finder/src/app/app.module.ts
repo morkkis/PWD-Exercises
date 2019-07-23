88e1b63c64ae1a3ca16f7d01c0769308a011d535
Nahari, Dan (dn415q) <dn415q@intl.att.com>
2019-07-23 14:42:48 +0300

    Break layout to components

--- Exercise-4/cat-finder/src/app/app.module.ts
+++ Exercise-4/cat-finder/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { CardsViewComponent } from './components/cards-view/cards-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
+    CardsViewComponent
   ],
   imports: [
     BrowserModule,
--- Exercise-5/cat-finder/src/app/app-routing.module.ts
+++ Exercise-5/cat-finder/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CardsViewComponent } from './components/cards-view/cards-view.component';
 
 
-const routes: Routes = [
-  { path: '', component: CardsViewComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

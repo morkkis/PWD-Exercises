96325fd1a3860451c9be9cd060c4a4a2d0b0a479
Nahari, Dan (dn415q) <dn415q@intl.att.com>
2019-07-23 15:12:25 +0300

    Add style card components + add navigator

--- Exercise-5/cat-finder/src/app/app.module.ts
+++ Exercise-5/cat-finder/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { CardsViewComponent } from './components/cards-view/cards-view.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
+    CardsViewComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,

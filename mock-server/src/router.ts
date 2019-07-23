9d5f30310de5f8eb455f2e222405d98f901da976
Nahari, Dan (dn415q) <dn415q@intl.att.com>
2019-07-23 13:20:12 +0300

    update mock api

--- mock-server/src/router.ts
+++ mock-server/src/router.ts
@@ -6,11 +6,24 @@ const router: CoreRouter = Router();
 router.get('/api/getCatList', async (req: Request, res: Response) => {
   console.log('-Get /api/getCatList');
   const data = (await import('./api/cat-list.json')).default;
-  // res.status(409).send((await import('./api/jasper/create_account_error_409.json')).default);
-  /*setTimeout(()=>{
-    res.json(data);
-  }, 5000);*/
   res.json(data);
+});
+
+router.delete('/api/deleteCat/:id', async (req: Request, res: Response) => {
+  console.log(`-Delete /api/removeCat/${req.params.id}`);
+  if ((+req.params.id) < 5) {
+	  res.status(409).send();
+  } else {
+	  setTimeout(() => {
+		res.status(204).send();
+	  }, 3000);
+  }
+});
+router.put('/api/addToFavorite/:id', async (req: Request, res: Response) => {
+  console.log(`-Put /api/addToFavorite/${req.params.id}`);
+  setTimeout(() => {
+	res.status(204).send();
+  }, 3000);
 });
 
 export default router;
